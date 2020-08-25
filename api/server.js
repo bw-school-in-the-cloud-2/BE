035cd176fc31396c83c442973e2fdfc212c4fa84
antonio martinez baez <tono.mtzb@gmail.com>
2020-08-25 13:48:32 -0500

    added schema for calendar functionality and country

--- api/server.js
+++ api/server.js
@@ -2,13 +2,14 @@ const express = require('express');
 const server = express();
 
 const authRouter = require('../auth/authRouter')
+const protected = require('../utils/protected')
 
 server.use(express.json());
 
 server.use('/api/auth', authRouter)
 
 
-server.get('/', (req, res) => {
+server.get('/',  (req, res) => {
   res.status(200).json({api: 'up'})
 })
 
