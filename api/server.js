78e9e9688e0c291be2372746b09a0783424f5465
antonio martinez baez <tono.mtzb@gmail.com>
2020-08-25 17:38:03 -0500

    implemented get request for volunteers

--- api/server.js
+++ api/server.js
@@ -2,11 +2,13 @@ const express = require('express');
 const server = express();
 
 const authRouter = require('../auth/authRouter')
+const volunteersRouter = require('../volunteers/volunteersRouter')
 const protected = require('../utils/protected')
 
 server.use(express.json());
 
 server.use('/api/auth', authRouter)
+server.use('/api/volunteers', volunteersRouter)
 
 
 server.get('/',  (req, res) => {
